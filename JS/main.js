7850e34fadc3a1fc70b2b6db6ab022c33ed71f75
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2021-01-07 10:08:33 -0300

    First commit

---
+++ JS/main.js
@@ -0,0 +1,13 @@
+var nome = "Mariana Caetano";
+var idade = 5;
+var idade2 = 2;
+var soma = idade + idade2
+var frase = "BTS Fanchant"
+
+//alert(nome +" tem " + idade + " anos.");
+//alert(idade + " + " + idade2 + " = " + soma);
+console.log(nome);
+console.log(idade + idade2);
+console.log(frase.toLowerCase());
+console.log(frase.replace("Fanchant" , "is " + soma + " years old"))
+//alert(frase.replace("Fanchant" , "is great!"));
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # aula-js
 JavaScript introduction class 
+
+## Goals
+Study and practice:
+
+ -console.log
+ -alert
+ -Arrays
+ -conditionals, loops and date
