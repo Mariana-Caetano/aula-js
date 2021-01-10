5501547e9b8e9d951e30827e11ea6449b430c15c
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2021-01-09 23:35:07 -0300

    array exercises

--- JS/main.js
+++ JS/main.js
@@ -1,13 +1,25 @@
-var nome = "Mariana Caetano";
-var idade = 5;
-var idade2 = 2;
-var soma = idade + idade2
-var frase = "BTS Fanchant"
+//var nome = "Mariana Caetano";
+//var idade = 5;
+//var idade2 = 2;
+//var soma = idade + idade2
+//var frase = "BTS Fanchant"
 
 //alert(nome +" tem " + idade + " anos.");
 //alert(idade + " + " + idade2 + " = " + soma);
-console.log(nome);
-console.log(idade + idade2);
-console.log(frase.toLowerCase());
-console.log(frase.replace("Fanchant" , "is " + soma + " years old"))
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.toLowerCase());
+//console.log(frase.replace("Fanchant" , "is " + soma + " years old"))
 //alert(frase.replace("Fanchant" , "is great!"));
+
+
+//Array:
+
+var lista = ["maçã", "uva", "melância"];
+//lista.push("laranja") //add another element on the list. 
+//lista.pop(); //retira o último item da lista
+//console.log(lista);
+
+console.log(lista.length); //elements quantity on the list
+//console.log(lista[2]); //only the position 2 will be printed
+//alert(lista[2] + " é a fruta escolhida"); 
