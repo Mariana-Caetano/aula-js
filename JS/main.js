23a5abfa47a68b2a9168a9d06a9219fda2704047
Mariana Caetano Pereira <nanah.caetano@gmail.com>
2021-01-13 10:54:27 -0300

    age validation

--- JS/main.js
+++ JS/main.js
@@ -77,8 +77,17 @@ function soma(n1, n2) {
     return n1 + n2
 }
 
-function setReplace(frase, nome, nome2) {
-    return frase.replace("nome, nome2")
+function validaIdade(idade) {
+    var validar ;
+    if(idade >= 18){
+        validar = true
+    }else{
+    validar = false
+    }
+    return validar;
 }
+
+var idade = prompt("Qual a sua idade?")
+console.log(validaIdade(idade));
+
 //alert(soma(5, 10));
-alert(setReplace("Bem vinde", "pequene", "gafanhoto"));
